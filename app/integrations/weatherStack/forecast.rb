a477ce3fd6b885139143fca9f17be2eabef57a03
celestialskys <lianne.bisch@gmail.com>
2025-04-15 15:32:29 -0400

    fixing access token retrieve;

--- app/integrations/weatherStack/forecast.rb
+++ app/integrations/weatherStack/forecast.rb
@@ -2,8 +2,11 @@ module WeatherStack
   class Forecast < Base
   
     def get_forcast(options={})
-          
-      result = request(:get, options[:params][:path], { params: options[:params] || {}})
+      byebug
+      params ={
+        access_key: @access_token
+      }.merge(params[:options] || {})
+      result = request(:get, options[:params][:path], { params: params })
     
       parsed_result = IntegrationsBase.parse_json_response(result)
       return parsed_result if parsed_result[:success]
